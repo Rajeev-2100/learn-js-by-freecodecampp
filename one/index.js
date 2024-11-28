fecd7079e0668e60cf0da2ce3cbca11e94ecc2e2
Rajeev-2100 <rajeevrawat2100@gmail.com>
2024-11-28 12:37:51 +0000

    first code is to create a variable

---
+++ one/index.js
@@ -0,0 +1,46 @@
+// document.getElementById("count-el").innerText = 5
+
+
+let count = 5
+
+// console.log(count);
+
+// 1:> Create a variable, myAge and set its value to your age 
+
+let myAge = 22
+
+
+// 2:> Log the myAge variable to the console
+
+console.log(myAge)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
