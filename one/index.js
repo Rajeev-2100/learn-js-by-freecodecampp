e47087fd8e80533d125064bc17071234822287b0
Rajeev-2100 <rajeevrawat2100@gmail.com>
2024-11-29 14:26:46 +0000

    mathematical Operation find out the myDogAge

--- one/index.js
+++ one/index.js
@@ -7,12 +7,23 @@ let count = 5
 
 // 1:> Create a variable, myAge and set its value to your age 
 
-let myAge = 22
+// let myAge = 22
 
 
 // 2:> Log the myAge variable to the console
 
-console.log(myAge)
+// console.log(myAge)
+
+// 1. Create two variables, myAge and humanDogRatio 
+// 2. Multiply the two together and store the result in myDogAge
+// 3. Log myDogAge to the console
+
+let myAge = 22
+let humanDogRatio = 7 
+
+let myDogAge = myAge * humanDogRatio
+
+console.log("My Dog Age: ", myDogAge)
 
 
 
