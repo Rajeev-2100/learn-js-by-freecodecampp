2469e1fb622be8224a73e16703464c660c64fad2
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-01 23:05:28 +0530

    Create a function then only print into console

--- one/index.js
+++ one/index.js
@@ -1,28 +1,18 @@
 // document.getElementById("count-el").innerText = 5
 
 
-
-// Create a variable. bonusPoints. Initialize it as 50. Increase it to 
-// 100. Decrease it down to 25, and then finally increase it to 70
-// console the value after each step
-
-let bonusPoints = 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
-
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+// initalize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button
+//  is clicked change the count-el in the HTML to reflect the new cound
 
 
 
+let count = 0
 
-
-
+function increment() {
+    console.log("The button was clicked")
+}
 
 
 
