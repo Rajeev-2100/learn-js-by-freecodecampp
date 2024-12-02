fdbbdf2ecec5b928b92180e61266b762513c7349
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-02 19:30:21 +0530

    create a first function

--- one/index.js
+++ one/index.js
@@ -1,25 +1,14 @@
 // document.getElementById("count-el").innerText = 5
 
-
-// initalize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button
-//  is clicked change the count-el in the HTML to reflect the new cound
-
-
-
-let count = 0
-
-function increment() {
-    console.log("The button was clicked")
+function countdown() {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
 }
 
-
-
-
-
-
-
+countdown()
 
 
 
