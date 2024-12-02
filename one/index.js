0933fb11be20994f55f3be294a258a1b4ec1708a
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-02 20:08:03 +0530

    Increment the variable using incrementlaps function

--- one/index.js
+++ one/index.js
@@ -1,16 +1,22 @@
-let lap1 = 14
-let lap2 = 53
-let lap3 = 63
+let lapsCompleted = 0
+
+// Create a function that increment the lapsCompleted variable with one 
+// Run it three times
 
 
-// Create a function that logs out the sum of all lap times
-
-function laptimes() {
-    let sum = lap1 + lap2 + lap3
-    console.log(sum)
+function incrementlaps() {
+    lapsCompleted = lapsCompleted + 1
+    console.log(lapsCompleted)
 }
 
-laptimes()
+call()
+call()
+call()
+
+
+
+
+
 
 
 
