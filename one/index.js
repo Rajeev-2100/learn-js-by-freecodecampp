2a28be973ea7736384ea0092ff883df5829c8d6f
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-02 20:57:40 +0530

    create a increment and decrement button when they clicked

--- one/index.js
+++ one/index.js
@@ -1,17 +1,32 @@
-let lapsCompleted = 0
 
-// Create a function that increment the lapsCompleted variable with one 
-// Run it three times
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count varible when the button is clicked 
+//(log it out) change the count-el in the HTML to reflect the new count
 
+let count = 0
 
-function incrementlaps() {
-    lapsCompleted = lapsCompleted + 1
-    console.log(lapsCompleted)
+function increment() {
+    count = count + 1
+    // console.log(count)
+    document.getElementById("count-el").innerText = count
 }
 
-call()
-call()
-call()
+
+function decrement(){
+    count = count - 1
+    document.getElementById("count-el").innerText = count
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
