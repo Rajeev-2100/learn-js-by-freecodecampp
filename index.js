5d3bbf90c2cc78f814b82a68fb5f5cb92e7b09f3
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-04 19:34:53 +0530

    add emoji this previous

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-// Grab the welcome-el paragraph and store it in a variable called welcomeEl
-
-// Create two variables (name & greeting) that contains your name
-// and the greeting we want to render on the page
-
-// Render the welcome message using welcomeEl.innerText
 
 let name = "Rajeev Rawat"
 
-let greeting = "Hi, my name is "
-let myGreeting = greeting + name
+let greeting = "Hi, My name is "
+let myGreeting = greeting + name + "🙏"
 // console.log(myGreeting)
 
 document.getElementById('welcome-el').innerText = myGreeting
+
+
+// Add an emoji to the end!
+// WRITE YOUR CODE BELOW HERE
+// HINT: count = count + 1
 
 
 
