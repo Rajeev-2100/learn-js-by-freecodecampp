a11fb459b0034a9665acb7dc22b0fb713902da71
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-05 22:04:11 +0530

    concatenate variable related problem done

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
-let saveEl = document.getElementById("save-el")
+// Create two variables, firstName and lastName
 
-let countEl = document.getElementById("count-el")
-let count = 0
+// Concatenate the two variables into a third variable called fullName
 
-// console.log(saveEl)
+// Log fullName to the console
 
-function increment() {
-    count += 1
-    countEl.innerText = count
-}
+let firstName = "Rajeev"
+let lastName = "Rawat"
 
-function save() {
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    let countStr =  count + " - " 
-    // 3. Render the variable in the saveEl using innerText
-    saveEl.textContent += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
-    countEl.textContent = 0
-    count = 0
-    console.log(count)
-}
+let fullName = firstName + " " + lastName
 
-
-
+console.log(fullName)
 
 
 
