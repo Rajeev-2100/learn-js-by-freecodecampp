323af0bc5a796cd6c2456c5dcb6c513776e9facb
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-07 22:05:20 +0530

    Create a simple calculator

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-// When the user clicks the purchase button, render out
-// "Something went wrong, please try again" in the paragraph
-// that has the id="error".
+let num1 = 10
+let num2 = 2
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
 
-let hi = document.getElementById("error")
-
-function purchase(){
-    // console.log("button is clicked")
-    hi.textContent = "Something went wrong, please try again"
+// Create four functions: add(), subtract(), divide(), multiply()
+let sumEl = document.getElementById('sum-el')
+function add() {
+    let result = num1 + num2
+    sumEl.textContent = "Sum: " + result
+    console.log(sum)
 }
 
+function subtract() {
+    let result = num1 - num2
+    sumEl.textContent = "Sum: " + result
+    console.log(subtract)
+}
+function multiply() {
+    let result = num1 * num2
+    sumEl.textContent = "Sum: " + result
+    console.log(multiply)
+}
 
+function divide() {
+    let result = num1 / num2
+    sumEl.textContent = "Sum: " + result
+    console.log(divide)
+}
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+
+add()
+subtract()
+multiply()
+divide()
+
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
 
 
 
