8ff4976f466cae29a4a79b933cac408688bcc17e
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-09 22:12:03 +0530

    Display the message and show in the message variable

--- index.js
+++ index.js
@@ -5,20 +5,22 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
+// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById('message-el')
 
-function startGame(){
+function startGame() {
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack!"
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
+    // console.log(message)    
 }
 
 
