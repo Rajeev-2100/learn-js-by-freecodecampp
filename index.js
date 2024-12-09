af652c0f8450e35b6336fa5e891770c25504bf22
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-09 23:00:47 +0530

    Create a new card button the add css and make message using function

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-// 2. Store the cards paragraph in a variable called cardsEl
-let cards = document.getElementById('cards-el')
+let cardsEl = document.getElementById("cards-el")
+
 function startGame() {
-    // 3. Render the cards on the page using this format -> "Cards: 10 4"
-    cards.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -22,6 +21,12 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+
+function newCard(){
+    messageEl.textContent = "Drawing a new card from the deck!"
 }
 
 
