4c5e163cbaa68751ecdfea5ea4c849952a249896
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-09 21:55:54 +0530

    Create start button then write function using with onclick event

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
 let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+function startGame(){
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
 }
-
-console.log(message);
-
-// 3. Log it out!
 
 
 
