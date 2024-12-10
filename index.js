2d65761834240c94227f8ec5adfff2256aaf8fe2
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-10 21:51:35 +0530

    Introduce the array

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard 
-let cards = [firstCard, secondCard]//array
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+// Arrays - ordered lists of items
 
-function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
-}
+let featuredPosts = [
+    "Check out my Netflix clone",
+    "Here's the code for my project",
+    "I've just relaunched my portfolio"
+]
 
-// 2. Create a new function called startGame() that calls renderGame()
-
-function startGame (){
-    renderGame()
-}
-function newCard(){
-    messageEl.textContent = "Drawing a new card from the deck!"
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 7
-    // 2. Add the new card to the sum variable
-    sum += card
-    renderGame()
-    startGame()
-}
+// Create an array that lists your i.e. experience, education, licenses, skills or similar
+// The items of the array should be strings
 
 
+let listItems = ["Fresher","Dipmola PassOut in IT","No Licenses","Full stack developer"]
 
 
 
