50e9d4e3e3ebd533e50ea7dc5bf5745048e393ef
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-10 22:45:27 +0530

    practice problem of loop is done

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-// Count to ten!
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
 
-// We need to specify...
-
-// Where should we START counting?
-// Where is the FINISH line?
-// What's the STEP SIZE we should use?
-
-//    START           FINISH       STEP SIZE
-for ( let count = 1;  count < 11;  count += 1 )  {
-    
-    console.log(count)
-
+for (let i = 10; i < 100; i += 10) {
+    console.log(i);
 }
+
 
 
 
