360701ba2f787820fed2734c568499fa4874b860
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-11 23:15:08 +0530

    Create a logic of Random number in lodu game

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-let randomNumber = Math.floor(Math.random() * 6)
+// Try to modify the expression so that we get a range from 1 to 6.
+let randomNumber = Math.floor(Math.random() * 6) + 1
 
-
-console.log(flooredNumber)
-
-
-/* 
-
-Write down all the possible values randomNumber can hold now!
-
-number between 1 to 5
-
-*/
-
+console.log(randomNumber);
 
 
