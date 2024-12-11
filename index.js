3b2e3418f244a26ce1871681df7ff231d8782a10
Rajeev-09 <rajeevrawat2100@gmail.com>
2024-12-11 22:37:59 +0530

    Create two type of function which has same meaning

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
-let sentence = ["Hello", "my", "name", "is", "Per"] 
-let greetingEl = document.getElementById("greeting-el")
+let player1Time = 102
+let player2Time = 107
 
-// how do you keep the spaces between the words if I remove them the array?
-
-for (let i = 0; i < sentence.length; i++){
-    greetingEl.textContent += sentence[i] + " ";
+function getFastestRaceTime() {
+    if (player1Time < player2Time) {
+        return player1Time
+    } else if (player2Time < player1Time) {
+        return player2Time
+    } else {
+        return 0
+    }
 }
 
+let fastestRace = getFastestRaceTime()
+// console.log(fastestRace);
 
 
+// Write a function that returns the total race time
+// Call/invoke the function and store the returned value in a new variable
+// Finally, log the variable out
 
+function call() {
+    let totalRaceTime = player1Time + player2Time
+    console.log(totalRaceTime);
+    return totalRaceTime
+}
 
+call()
 
+function getTotalRaceTime(params) {
+    return player1Time + player2Time
+}
 
+let totalRaceTime = getTotalRaceTime()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(totalRaceTime);
